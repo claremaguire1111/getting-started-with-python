9b5b65225fe07ea1b630179483651ca717e4bf07
Clare_Maguire <clare@thinkblendlabs.com>
2024-09-24 21:34:18 -0500

    Updated get-creative-with-the-turtle-module

--- get-creative-with-the-turtle-module/main.py
+++ get-creative-with-the-turtle-module/main.py
@@ -3,11 +3,13 @@ import random
 
 print("Welcome to your drawing app! Select a shape you want to draw. After typing the number and hitting enter, go to the result tab!")
 
+
 turtle.penup()
 turtle.left(90)
 turtle.forward(250)
 turtle.left(90)
 turtle.forward(300)
+
 turtle.pendown()
 
 turtle.write("Drawing App!", font=("Arial", 16, "normal"))
@@ -16,6 +18,7 @@ turtle.penup()
 turtle.left(90)
 turtle.forward(50)
 turtle.pendown()
+
 
 turtle.write("To draw a shape, go to the Console tab and choose an option!", font=("Arial", 16, "normal"))
 
@@ -27,12 +30,23 @@ turtle.pendown()
 
 def star():
   # Star
+  for i in range(0,5):
+    turtle.pencolor("red")
+    turtle.forward(110)
+    turtle.right(216)
 
 def square():
   # Square
+  for i in range(0,4):
+    turtle.pencolor("orange")
+    turtle.forward(100)
+    turtle.right(90)
 
 def hexagon():
-  # Hexagon
+  for i in range(0,6):
+    turtle.pencolor("green")
+    turtle.forward(100)
+    turtle.right(60)
 
 selection = input("1. Star\n2. Square\n3. Hexagon\nSelect a number: ")
 if selection == "1":
